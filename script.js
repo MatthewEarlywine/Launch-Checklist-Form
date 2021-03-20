0ba82fd59d11f1a7448fc1aabc5345b5bf63c622
Matt <matthewearlywine@gmail.com>
2021-03-20 15:25:33 -0500

    Index 003. Script 003.

--- script.js
+++ script.js
@@ -16,6 +16,14 @@ window.addEventListener("load", function() {
       let fuelLevelInput = document.getElementById("fuelLevel");
       let cargoMassInput = document.getElementById("cargoMass");
 
+      let pilotStatusInput = document.getElementById("pilotStatus");
+      let copilotStatusInput = document.getElementById("copilotStatus");
+      let fuelStatusInput = document.getElementById("fuelStatus");
+      let cargoStatusInput = document.getElementById("cargoStatus");
+
+      let launchStatusOutput = document.getElementById("launchStatus");
+      let launchChecklist = document.getElementById("faultyItems");
+      
       if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || 
       cargoMassInput.value === ""){
          alert("All fields are required!");
@@ -37,11 +45,40 @@ window.addEventListener("load", function() {
          alert("Listed cargo mass is not a number!");
          event.preventDefault()
       }
+
+      pilotStatusInput.innerHTML = pilotNameInput.value;
+      copilotStatusInput.innerHTML = copilotNameInput.value;
+      
+      if (fuelLevelInput.value < 10000){
+         launchChecklist.style.visibility = "visible";
+         fuelStatusInput.innerHTML = "Not enough fuel for the journey.";
+         launchStatusOutput.innerHTML = "Shuttle not ready for launch.";
+         launchStatusOutput.style.color = "red";
+      }
+
+      if (cargoMassInput.value > 10000){
+         launchChecklist.style.visibility = "visible";
+         cargoStatusInput.innerHTML = "Too much mass for takeoff.";
+         launchStatusOutput.innerHTML = "Shuttle not ready for launch.";
+         launchStatusOutput.style.color = "red";
+      }
+
+      if (fuelLevelInput.value >= 10000 && cargoMassInput.value <= 10000){
+         launchChecklist.style.visibility = "visible";
+         fuelStatusInput.innerHTML = "Fuel level high enough for launch."
+         cargoMassInput.innerHTML = "Cargo mass low enough for launch."
+         launchStatusOutput.innerHTML = "Shuttle is ready for launch.";
+         launchStatusOutput.style.color = "green";
+      }
+      
+      
+      
+      event.preventDefault();
    
 
 
-   });
-});
+   }); 
+});  
 
 
 
